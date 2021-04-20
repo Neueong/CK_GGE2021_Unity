a5ba4b0fc86244c4fc421ccf3aed8f17003807de
One <lswsox@gmail.com>
2021-04-21 05:16:42 +0900

    Standard Asset 업데이트

---
+++ Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+#pragma warning disable 618
+namespace UnityStandardAssets.Utility
+{
+    public class SimpleActivatorMenu : MonoBehaviour
+    {
+        // An incredibly simple menu which, when given references
+        // to gameobjects in the scene
+        public Text camSwitchButton; // 높은 버전의 유니티에서 Standard Asset을 불러왔을 때 'GUIText' is obsolete 에러 뜨면서 UI.Text로 변경
+        public GameObject[] objects;
+
+
+        private int m_CurrentActiveObject;
+
+
+        private void OnEnable()
+        {
+            // active object starts from first in array
+            m_CurrentActiveObject = 0;
+            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+        }
+
+
+        public void NextCamera()
+        {
+            int nextactiveobject = m_CurrentActiveObject + 1 >= objects.Length ? 0 : m_CurrentActiveObject + 1;
+
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].SetActive(i == nextactiveobject);
+            }
+
+            m_CurrentActiveObject = nextactiveobject;
+            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+        }
+    }
+}
